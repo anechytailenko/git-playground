8c455b82a97256fc33da4774fff1fde7d46f668b
AnnN <anna.nechytailenko@gmail.com>
2024-11-03 10:28:43 +0200

    Display message in case of lost

--- main.py
+++ main.py
@@ -61,5 +61,8 @@ while not is_game_over():
         print(f"That's right! {WORDS_TO_WIN - guessed} to go")
     else:
         errors += 1
-        print(f"Oops :( No such word, you have {ERRORS_TO_LOSE - errors} lives more")
+        if errors == ERRORS_TO_LOSE:
+            print("You lost! GAME OVER")
+        else:
+            print(f"Oops :( No such word, you have {ERRORS_TO_LOSE - errors} lives more")
 
